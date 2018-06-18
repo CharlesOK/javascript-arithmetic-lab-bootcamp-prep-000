72e2ea0f607ce6215a26937acf3bdd4e049cd992
charlesokwechime@gmail.com Okwechime <charlesokwechime@gmail.com>
2018-06-18 05:23:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 function add(a, b) {
   return a + b;
 }
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function add(a, b) {
+  return a + b;
+}
